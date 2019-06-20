94a732c51d6110030c71941a00676ac4ffb567f4
ew mini3 <ewmini3@Renzo.lan>
2019-06-20 15:07:40 -0300

    Replaced double by single quotes, deleted scriptOB.js Amend to apply new changes

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,6 @@ module MonthlyTasks
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.assets.paths << Rails.root.join("app", "assets", "fonts") # added custom fonts
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts') # added custom fonts
   end
 end
