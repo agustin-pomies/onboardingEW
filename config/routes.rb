0cc71f2d902c52f3d22c4e9caf42e9fd2a4b625f
ew mini3 <ewmini3@Renzo.lan>
2019-06-24 13:52:34 -0300

    Collaborators can be invited to help in tasks

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index]
   resources :tasks
-  resources :tasks do
-    post :update_multiple, on: :collection
-  end
   root 'tasks#index'
 end
