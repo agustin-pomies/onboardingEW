72890e6961c61f06f46834940d62778ade184114
ew mini3 <ewmini3@Renzo.lan>
2019-06-25 16:07:10 -0300

    My profile section created

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'user/show'
   get 'static_pages/instructions'
   devise_for :users
   resources :tasks
   get '/page_use', to: 'static_pages#instructions', as: 'instructions'
+  get '/profile', to: 'user#show', as: 'profile'
   root 'tasks#index'
 end
